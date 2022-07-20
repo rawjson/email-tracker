34d3b5b81a4059b8f25700912a5f5a0a1324de67
navdesign <navninderbenipal@gmail.com>
2022-07-20 14:09:52 +0530

    added example env

--- src/__app.module.ts
+++ src/__app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import EmailModule from './email/__module';
 import SubscriberModule from './subscribers/__module';
 import { AppController } from './__app.controller';
 import { AppService } from './__app.service';
 import { DatabaseModule } from './__db.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), SubscriberModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({}),
+    SubscriberModule,
+    DatabaseModule,
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

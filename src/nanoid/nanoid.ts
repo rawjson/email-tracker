35a9a615bce66c054b4d1d3477c61f247f740ce5
navdesign <navninderbenipal@gmail.com>
2022-08-11 12:01:35 +0530

    code changes. Updated the nanoid file. I am also thinking of using the cyrpto module. That is for later.

--- src/nanoid/nanoid.ts
+++ src/nanoid/nanoid.ts
@@ -1,6 +1,6 @@
-export const nanoid = () => {
-  let result = '';
-  const chars =
+export const nanoid = (): string => {
+  let result: string = '';
+  const chars: string =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < 24; i++) {
     result += chars.charAt(Math.floor(Math.random() * chars.length));
